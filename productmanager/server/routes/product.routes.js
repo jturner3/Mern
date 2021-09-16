9ee18c855d9c8546f9e5fbfcd6418f3519f2e997
jturner3 <43verventures.io>
2021-09-16 18:50:52 -0400

    file fixing

--- productmanager/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require("../controllers/product.controller");
-
-module.exports = app => {
-    app.get("/api/products/", ProductController.findAllProducts);
-    app.post("/api/products/", ProductController. createNewProduct);
-    app.get("/api/products/:id", ProductController.findOneProduct);
-    app.put("/api/products/:id/edit", ProductController.updateExistingProduct);
-    app.delete("/api/products/:id", ProductController.deleteExistingProduct);
-}
